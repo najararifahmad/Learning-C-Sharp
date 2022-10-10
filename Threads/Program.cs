7ff9f80a315b0d92d8c1143ef1cd5383ceac1382
Arif Ahmad Najar <najararifahmad@gmail.com>
2022-10-10 23:23:20 +0530

    Add project files.

---
+++ Threads/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Program
+    {
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart ts = new ThreadStart(ThreadMethod);
+            Thread t = new Thread(ts);
+            t.Start();
+            for (int i = 0; i < 400; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+            }
+            t.Join();
+            Console.ReadKey();
+        }
+    }
+}
